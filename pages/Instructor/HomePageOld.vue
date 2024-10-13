07bba2ad25c4fab0a2d517c2a7674a23ccf85c14
Yu Chen <yeoyuchen@gmail.com>
2024-10-13 16:18:20 +0800

    dashboard look

---
+++ pages/Instructor/HomePageOld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "instructorview",
+});
+
+</script>
+
+<template>
+    <div class="space-y-0.5">
+      <h2 class="text-2xl font-bold tracking-tight">
+        Homepage
+      </h2>
+      <p class="text-muted-foreground">
+        See your students!
+      </p>
+    </div>
+</template>
+
+<style scoped>
+/* Add any homepage-specific styling */
+</style>
