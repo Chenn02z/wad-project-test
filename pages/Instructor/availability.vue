25ea586694132181310b4e20e2cb0a2f43559c6e
brennantoh <brennan.toh.2023@scis.emu.edu.sg>
2024-10-31 11:01:48 +0800

    made availability wider

--- pages/Instructor/availability.vue
+++ pages/Instructor/availability.vue
@@ -1,19 +1,18 @@
 <template>
-  
-    <div class="space-y-0.5">
-      <h2 class="text-2xl font-bold tracking-tight">
-        Availability
-      </h2>
-      <p class="text-muted-foreground">
-        Select the days and timeslots
-      </p>
-    </div>
-    <div class="p-6 max-w-md mx-auto">
+  <div class="space-y-0.5">
+    <h2 class="text-2xl font-bold tracking-tight">
+      Availability
+    </h2>
+    <p class="text-muted-foreground">
+      Select the days and timeslots
+    </p>
+  </div>
+  <div class="p-6 mx-auto w-full max-w-screen-lg"> 
     <DaySelector />
     <TimeSlots />
   </div>
-
 </template>
+
 
 <script setup lang="ts">
 definePageMeta({
